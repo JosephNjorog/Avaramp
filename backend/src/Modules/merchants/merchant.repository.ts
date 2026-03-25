5793495bf2e4ba578da21cdc644f0c98f63e5909
Ess <127221085+EstherLavender@users.noreply.github.com>
2026-03-25 18:01:12 +0300

    Create merchant.repository.ts

---
+++ backend/src/Modules/merchants/merchant.repository.ts
@@ -0,0 +1,9 @@
+
+cat > backend/src/modules/merchants/merchant.repository.ts << 'EOF'
+import { prisma } from "../../shared/database/prisma";
+export class MerchantRepository {
+  async create(data: any)            { return prisma.merchant.create({ data }); }
+  async findById(id: string)         { return prisma.merchant.findUnique({ where: { id } }); }
+  async findByEmail(email: string)   { return prisma.merchant.findUnique({ where: { email } }); }
+  async findAll()                    { return prisma.merchant.findMany({ where: { isActive: true } }); }
+}
